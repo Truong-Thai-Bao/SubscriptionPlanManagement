e9c1fb441333310bba0c874f0c9b176ffd00c94c
tutai13 <taidtpd10687@gmail.com>
2026-04-03 14:53:16 +0700

    feature Allow user to Change Password

--- services/CoreLMS-service/src/app.js
+++ services/CoreLMS-service/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const courseRoute = require('./routes/courseRoute.js');
-
+const authRoutes = require('./routes/authRoute');
 
 const app = express();
 
-
+app.use(express.json());
 app.use('/api/course',courseRoute);
 
+app.use('/api/auth', authRoutes);
 module.exports = app;
